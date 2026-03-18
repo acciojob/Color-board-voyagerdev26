1d03bd9ce4165573e843005e231cbc729899d9fc
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-03-18 14:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let container=document.querySelector(".container");
+for(let i=1 ;i<=800;i++){
+	let box=document.createElement("div");
+	box.addEventListener("mouseover",(e)=>{
+		let rc=()=>{return Math.floor(Math.random()*256)};
+		box.style.backgroundColor=`rgb(${rc()},${rc()},${rc()})`
+	})
+	box.addEventListener("mouseout",(e)=>{
+		box.style.transition="1s";
+		box.style.backgroundColor=`rgb(${29},${29},${29})`;
+	})
+	
+	box.className="square";
+	container.append(box);
+}
+
